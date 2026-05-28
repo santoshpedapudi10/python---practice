a33ca5cb567f3907dda9a3a81b02f3ce2213683d
santoshpedapudi10 <santoshpedapudi10@gmail.com>
2026-05-28 14:53:43 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,11 @@
+# Python Variables Practice
+
+student_name = "Santosh Pedapudi"
+age = 21
+course = "Data Science"
+learning_platform = "GitHub"
+
+print("Student Name :", student_name)
+print("Age           :", age)
+print("Course        :", course)
+print("Platform      :", learning_platform)
